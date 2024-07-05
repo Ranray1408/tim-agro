10fcfaf60e7570cd04b1dba02e319b54f22b5f15
Kyrylo Kutetskyi <diozx5@gmail.com>
2024-07-05 11:30:33 +0300

    404 page

--- wp-content/themes/wp-rock/404.php
+++ wp-content/themes/wp-rock/404.php
@@ -10,10 +10,7 @@
 get_header();
 global $global_options;
 
-$page_404_background_image = get_field_value($global_options, 'page_404_background_image');
-$page_404_big_title = get_field_value($global_options, 'page_404_big_title');
 $page_404_title = get_field_value($global_options, 'page_404_title');
-$page_404_description = get_field_value($global_options, 'page_404_description');
 $page_404_link_text = get_field_value($global_options, 'page_404_link_text');
 
 do_action('wp_rock_before_page_content');
@@ -23,18 +20,12 @@ do_action('wp_rock_before_page_content');
         <div class="section-404__content">
 
             <?php
-            if (!empty($page_404_big_title)) {
-                echo '<h1 class="section-404__big-title">' . do_shortcode($page_404_big_title) . '</h1>';
-            }
             if (!empty($page_404_title)) {
                 echo '<h2 class="section-404__title">' . do_shortcode($page_404_title) . '</h2>';
             }
-            if (!empty($page_404_description)) {
-                echo '<p class="section-404__description">' . do_shortcode($page_404_description) . '</p>';
-            }
 
             if (!empty($page_404_link_text)) {
-                echo '<a class="section-404__link" href="' . esc_attr(get_home_url()) . '">
+                echo '<a class="section-404__link green-transparent" href="' . esc_attr(get_home_url()) . '">
                         ' . $page_404_link_text . '
                     </a>';
             }
